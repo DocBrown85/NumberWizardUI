ecbec78965fd562610ef341fbb4e03b9d2df4764
docbrown85 <me@example.com>
2020-04-26 16:07:50 +0200

    add Number Wizard UI source code

---
+++ Number WIzard UI/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        Application.Quit();
+    }
+}
